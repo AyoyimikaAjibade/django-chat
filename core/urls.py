47819ee26e0c336acb251ac798abcee8d93b1bf9
Ayoyimika <ayoyimika@quickcheck.ng>
2023-03-20 21:30:17 +0100

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from .views import frontpage, signup
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'), 
+    path('sign-up/', signup, name='signup'), 
+    path('log-out/', auth_views.LogoutView.as_view(), name='logout'), 
+    path('log-in/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'), 
+]
